cb5e63230cb79f50c06765a8edcb6ea2a7538c31
“justintangjx” <just.think.tang@gmail.com>
2018-05-12 15:49:30 +0800

    added logic to server to deploy on heroku

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const socket = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
-const server = app.listen(4000,function(){
-    console.log('listening to port 4000');
-}); 
+const PORT = 4000
+const server = app.use((req, res) => res.sendFile(INDEX)).listen(PORT, () => 
+  console.log(`listening to port ${PORT}`));
 
-app.use(express.static('client'));
 
+app.use(express.static("client"));
 
 // socket.io to work on this var server; will wait for client
 const io = socket(server);
+let connections = [];
+let users = [];
 // to listen to client on that ONE particular socket!!
-io.on('connection', function(socket) {
-    console.log('socket connection made with ' + socket.id);
-    socket.on('newMessage', function(data){
-        io.sockets.emit('newMessage', data);
-    });
+io.on("connection", function(socket) {
+  connections.push(socket);
+  console.log("Connected: %s sockets connected", connections.length);
+  socket.on("newMessage", function(data) {
+    io.sockets.emit("newMessage", data);
+  });
 
-    socket.on('typing', function(data){
-        // broadcast to all other sockets EXCEPT for the one typing!!!
-        socket.broadcast.emit('typing', data)
-    })
+  socket.on("typing", function(data) {
+    // broadcast to all other sockets EXCEPT for the one typing!!!
+    socket.broadcast.emit("typing", data);
+  });
 });
-
-
-
